aeb3a30f68100a0c9383bedcd00eaa696700636a
tnRaro <admin@tnraro.com>
2015-10-22 22:44:47 +0900

    Fixed typo.

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
 		loaders: [
 			{
 				test: /\.jsx?$/,
-				exclude: /{node_modules|bower_components}/,
+				exclude: /(node_modules|bower_components)/,
 				loader: "babel!eslint"
 			},
 			{
